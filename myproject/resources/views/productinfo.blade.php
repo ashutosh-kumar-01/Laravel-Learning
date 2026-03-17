9342b9be1777b29b08430487d594fde91da235bd
ashutosh7761 <goluashu3@gmail.com>
2026-03-17 21:20:44 +0530

    productinfo

---
+++ myproject/resources/views/productinfo.blade.php
@@ -0,0 +1,21 @@
+<h1>Electronic Products</h1>
+<table border = 1>
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Name</th>
+			<th>Price</th>
+            <th>Description</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach ($products as $product)
+		<tr>
+			<td>{{ $product['id'] }}</td>
+			<td>{{ $product['name'] }}</td>
+			<td>{{ $product['price'] }}</td>
+            <td>{{ $product['description'] }}</td>
+		</tr>
+		@endforeach
+	</tbody>
+</table>
--- myproject/routes/web.php
+++ myproject/routes/web.php
@@ -6,6 +6,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::get('/about', function () {
     return "This is the About page";
 });
@@ -60,8 +62,31 @@ Route::get('/admin',function(){
 });
 
 // passing data to the view with chaining method
-Route::get('/admin',function(){
+/* Route::get('/admin',function(){
     return view('adminpage')->with('','')->with('','')->with('','');
+}); */
+
+
+Route::get('/products', function() {
+    $products=[
+        ["id"=>1,"name"=>"AC","price"=>45000,"description"=>"Powerful air conditioning for instant cooling and comfort."],
+        ["id"=>2,"name"=>"Cooler","price"=>15000,"description"=>"Energy-efficient cooler for fresh, breezy air in summer."],
+        ["id"=>3,"name"=>"Fan","price"=>5000,"description"=>"Compact fan for quick and effective air circulation."]
+    ];
+    return view('productinfo', compact('products'));
 });
+
+
+// create a route to store usernam="ashutosh" as a cookie value for 30 
+// cookie is an info stored on client's browser
+// cookie value is stored in apllication layar
+// don't save confidential info as a cookie data
+
+Route::get('/set-cookie',function(){
+    $time=30;
+    return response('Cookie has been set')->cookie('username','ashutosh',$time);
+});
+
+
 
 
